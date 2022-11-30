cf087df67853841f212ebfa4b5518e16b28ff733
KseniyaShautsova <108619112+KseniyaShautsova@users.noreply.github.com>
2022-11-30 15:27:36 +0100

    added files

--- src/models/Playlist.model.ts
+++ src/models/Playlist.model.ts
@@ -1,3 +1,8 @@
+import { model, models, Schema } from "mongoose";
+import {number} from "prop-types";
+import { DEFAULT_CARD_COLOR } from "~/config/common.config";
+
+
 export interface PlaylistModel {
   color?: string;
   name: string;
@@ -7,4 +12,15 @@ export interface PlaylistModel {
   upvote: number;
 }
 
+const schema = new Schema<PlaylistModel>({
+  color: { type: String, default: DEFAULT_CARD_COLOR },
+  name: { type: String, required: true },
+  owner: { type: String, required: true },
+  slug: { type: String, required: true },
+  spotifyId: { type: String, required: true },
+  upvote: { type: Number, required: true },
+});
+
 export type PlaylistModelWithId = PlaylistModel & { id: string };
+
+export const PlaylistModel = models.PlaylistModel || model<PlaylistModel>("PlaylistModel", schema);
--- src/pages/api/students/[id].tsx
+++ src/pages/api/students/[id].tsx
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
+ import { NextApiRequest, NextApiResponse } from "next";
 import { dbConnect } from "~/libraries/mongoose.library";
 import { Student } from "~/models/Student.model";
 
